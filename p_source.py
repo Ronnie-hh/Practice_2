1e3ee2fcd36d7ad5915a3bf7e81bc2b5c6034981
Ronnie-hh <yejin0858@dongguk.edu>
2022-05-23 00:48:02 +0900

    ADD student univ in ossp

--- p_source.py
+++ p_source.py
@@ -1,6 +1,8 @@
 s_name = input('이름을 입력하세요: ')
 s_num = input('학번을 입력하세요: ')
+s_univ = input('대학교를 입력하세요: ')
 print('')
 print('<출력>')
 print(f'이름: {s_name}')
 print(f'학번: {s_num}')
+print(f'대학교: {s_univ}')
